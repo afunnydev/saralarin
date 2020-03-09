18e19253b31705cb6264848e2eaf9f3b1d78450a
Felix DB <the2deux@gmail.com>
2020-03-09 10:57:24 -0400

    Update dependencies and gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,5 @@
 const gulp = require('gulp');
-const imagemin = require("gulp-imagemin");
 const imageresize = require('gulp-image-resize');
-const parallel = require("concurrent-transform");
-var runSequence = require('run-sequence');
-var del = require('del');
 var exec = require('child_process').exec;
 var newer = require('gulp-newer');
 var sass = require('gulp-sass');
@@ -21,46 +17,44 @@ const imagequart = 600;
 const imageport = 300;
 const imagethumb = 80;
 const jsFiles = [
-                  'themes/saralarin/assets/js/theme/jquery.js',
-                  'themes/saralarin/assets/js/theme/modernizr.js',
-                  'themes/saralarin/assets/js/theme/bootstrap.min.js',
-                  'themes/saralarin/assets/js/theme/jquery.easing.1.3.js',
-                  'themes/saralarin/assets/js/theme/skrollr.min.js',
-                  'themes/saralarin/assets/js/theme/smooth-scroll.js',
-                  'themes/saralarin/assets/js/theme/jquery.appear.js',
-                  'themes/saralarin/assets/js/theme/bootsnav.js',
-                  'themes/saralarin/assets/js/theme/jquery.nav.js',
-                  'themes/saralarin/assets/js/theme/wow.min.js',
-                  'themes/saralarin/assets/js/theme/page-scroll.js',
-                  'themes/saralarin/assets/js/theme/swiper.min.js',
-                  'themes/saralarin/assets/js/theme/jquery.count-to.js',
-                  'themes/saralarin/assets/js/theme/jquery.stellar.js',
-                  'themes/saralarin/assets/js/theme/jquery.magnific-popup.min.js',
-                  'themes/saralarin/assets/js/theme/isotope.pkgd.min.js',
-                  'themes/saralarin/assets/js/theme/imagesloaded.pkgd.min.js',
-                  'themes/saralarin/assets/js/theme/classie.js',
-                  'themes/saralarin/assets/js/theme/hamburger-menu.js',
-                  'themes/saralarin/assets/js/theme/counter.js',
-                  'themes/saralarin/assets/js/theme/jquery.fitvids.js',
-                  'themes/saralarin/assets/js/theme/equalize.min.js',
-                  'themes/saralarin/assets/js/theme/skill.bars.jquery.js',
-                  'themes/saralarin/assets/js/theme/justified-gallery.min.js',
-                  'themes/saralarin/assets/js/theme/jquery.easypiechart.min.js',
-                  'themes/saralarin/assets/js/theme/instafeed.min.js',
-                  'themes/saralarin/assets/js/theme/retina.min.js',
-                  'themes/saralarin/assets/js/revolution/jquery.themepunch.tools.min.js',
-                  'themes/saralarin/assets/js/revolution/jquery.themepunch.revolution.min.js',
-                  'themes/saralarin/assets/js/theme/main.js',
-                  'themes/saralarin/assets/js/main.js'
-                ];
+  'themes/saralarin/assets/js/theme/jquery.js',
+  'themes/saralarin/assets/js/theme/modernizr.js',
+  'themes/saralarin/assets/js/theme/bootstrap.min.js',
+  'themes/saralarin/assets/js/theme/jquery.easing.1.3.js',
+  'themes/saralarin/assets/js/theme/skrollr.min.js',
+  'themes/saralarin/assets/js/theme/smooth-scroll.js',
+  'themes/saralarin/assets/js/theme/jquery.appear.js',
+  'themes/saralarin/assets/js/theme/bootsnav.js',
+  'themes/saralarin/assets/js/theme/jquery.nav.js',
+  'themes/saralarin/assets/js/theme/wow.min.js',
+  'themes/saralarin/assets/js/theme/page-scroll.js',
+  'themes/saralarin/assets/js/theme/swiper.min.js',
+  'themes/saralarin/assets/js/theme/jquery.count-to.js',
+  'themes/saralarin/assets/js/theme/jquery.stellar.js',
+  'themes/saralarin/assets/js/theme/jquery.magnific-popup.min.js',
+  'themes/saralarin/assets/js/theme/isotope.pkgd.min.js',
+  'themes/saralarin/assets/js/theme/imagesloaded.pkgd.min.js',
+  'themes/saralarin/assets/js/theme/classie.js',
+  'themes/saralarin/assets/js/theme/hamburger-menu.js',
+  'themes/saralarin/assets/js/theme/counter.js',
+  'themes/saralarin/assets/js/theme/jquery.fitvids.js',
+  'themes/saralarin/assets/js/theme/equalize.min.js',
+  'themes/saralarin/assets/js/theme/skill.bars.jquery.js',
+  'themes/saralarin/assets/js/theme/justified-gallery.min.js',
+  'themes/saralarin/assets/js/theme/jquery.easypiechart.min.js',
+  'themes/saralarin/assets/js/theme/instafeed.min.js',
+  'themes/saralarin/assets/js/theme/retina.min.js',
+  'themes/saralarin/assets/js/revolution/jquery.themepunch.tools.min.js',
+  'themes/saralarin/assets/js/revolution/jquery.themepunch.revolution.min.js',
+  'themes/saralarin/assets/js/theme/main.js',
+  'themes/saralarin/assets/js/main.js'
+];
 const jsDest = 'themes/saralarin/static/js';
-
  
 // resize and optimize images
 gulp.task("image-resize", () => {
   return gulp.src("themes/saralarin/source-images/*.{jpg,png,jpeg}")
     .pipe(newer("themes/saralarin/static/img"))
-    .pipe(imagemin())
     .pipe(imageresize({ width: imagexl}))
     .pipe(gulp.dest("themes/saralarin/static/xl/img"))
     .pipe(imageresize({ width: imagefull }))
@@ -76,38 +70,37 @@ gulp.task("image-resize", () => {
 });
 
 // hugo production call
-gulp.task("hugo", function (cb) {
-  exec('hugo --cleanDestinationDir', function (err, stdout, stderr) {
+gulp.task("hugo", (cb) => {
+  exec('hugo --cleanDestinationDir', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
   return gulp.src('themes/saralarin/assets/scss/main.scss')
-    // .pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename('main.min.css'))
-    // .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('maps'))
     .pipe(gulp.dest('themes/saralarin/static/css'));
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', () => {
     return gulp.src(jsFiles)
-        // .pipe(sourcemaps.init())
+        .pipe(sourcemaps.init())
         .pipe(concat('main.min.js'))
         .pipe(uglify())
-        // .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest(jsDest));
 });
 
 // watching
-gulp.task("watch", function() {
-
-  browserSync.init({
-      proxy: "http://localhost:1313/"
-  });
+gulp.task("watch", () => {
+  // browserSync.init({
+  //     proxy: "http://localhost:1313/"
+  // });
 
   gulp.watch('themes/saralarin/source-images/*.{jpg,png,jpeg,gif}', ['image-resize'] );
   gulp.watch('themes/saralarin/assets/scss/**/*.scss', ['sass']);
@@ -115,14 +108,7 @@ gulp.task("watch", function() {
 });
 
 // watching images and resizing
-gulp.task("dev",  function(callback) {
-  runSequence('image-resize',
-              'watch');
-});
+gulp.task("dev",  gulp.series('image-resize', 'watch'));
 
 // optimizing images and calling hugo for production
-gulp.task("prod",  function(callback) {
-  runSequence('image-resize',
-              'sass',
-              'hugo');
-});
+gulp.task("prod",  gulp.series('image-resize', 'sass', 'hugo'));
